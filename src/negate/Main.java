f45e1e8afe807676dd477bcebde11ba00df26c72
kloehnen <kloehn@email.arizona.edu>
2017-02-07 11:42:46 -0700

    Added Stanford Tags

--- src/negate/Main.java
+++ src/negate/Main.java
@@ -1,11 +1,12 @@
 package negate;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		TextRead foo = new TextRead(System.getProperty("user.dir") + "/resources/documents/AagenaesSyndrome.txt");
-
+		
 	}
 
 }
--- src/negate/TextRead.java
+++ src/negate/TextRead.java
@@ -1,66 +1,110 @@
 package negate;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-import edu.stanford.nlp.ling.HasWord;
-import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.util.*;
 
 public class TextRead {
+	
+	private List<ArrayList<String>> documentList = new ArrayList<ArrayList<String>>();	
+	
 	
 	public TextRead(String filepath) { 
 		
 		// Make a List of Array lists to save all sentences in the document
-		List<ArrayList<String>> documentList = new ArrayList<ArrayList<String>>();
+		String rawLine = null;
+		BufferedReader br = null;
+		FileReader fr = null;
 		
 		try {
 			
-			// Parse the Document into Sentences using the Stanford Preproccessor Class
-			DocumentPreprocessor dp = new DocumentPreprocessor(filepath);
+			// File Input Vars
+			fr = new FileReader(filepath);
+			br = new BufferedReader(fr);
 			
-			//
-			// Here we add all the Stanford Parer Tools we need -- NER?
-			// To preserve to fidelity of the original file
-			//
+			// creates a StanfordCoreNLP object, with POS tagging, parsing
+			Properties props = new Properties();
+			props.setProperty("annotators", "tokenize, ssplit, pos, parse");
+			StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 			
-			
-			// Transfer the Preprocessor to an Array List of Array Lists
-			for (List<HasWord> hwSentence : dp) {
+			// Read in File Buffer
+			br = new BufferedReader(new FileReader(filepath));
+
+			// Parse lines and annotate using Core NLP
+			while ((rawLine = br.readLine()) != null) {
 				
-				// Array list of strings for each sentence
-				 ArrayList<String> sentenceArray = new ArrayList<String>();
-				
-				for (HasWord hwTok : hwSentence) {
-					
-					// convert HasWord to String and add to Array
-					sentenceArray.add(hwTok.word());
-				
-				}
-				
-				// Add sentence array to document list
-				documentList.add(sentenceArray);
-			
+				Annotation antdLine = new Annotation(rawLine);
+			    // run all the selected Annotators on this text
+			    pipeline.annotate(antdLine);
+			    
+			    List<CoreMap> coreSents = antdLine.get(CoreAnnotations.SentencesAnnotation.class);
+			    
+			    for(CoreMap coreSent : coreSents) {
+			    	
+					// Array list of strings for each sentence
+					ArrayList<String> sentenceArray = new ArrayList<String>();
+
+			    	for (CoreLabel coreToken: coreSent.get(TokensAnnotation.class)) {
+			    		
+			    		// get the text of the token
+			    		String word = coreToken.get(TextAnnotation.class);
+			    		
+			    		// get pos tag of the token
+			    		String pos = coreToken.get(PartOfSpeechAnnotation.class);
+			    		
+			    		// Add the word and pos
+			    		String token = word + "\t" + pos;
+			    		
+			    		// Add token to Sentence Array List
+			    		sentenceArray.add(token);
+			    	}
+
+			    	// this is the parse tree of the current sentence
+			    	//Tree tree = coreSent.get(TreeAnnotation.class);
+			    	documentList.add(sentenceArray);
+			    }    
 			}
+			    
+			System.out.println("Works!");
 			
+		} catch (IOException e1) {
 			
-		} catch (Exception e1) {
 			// Couldn't open the File
 			e1.printStackTrace();
-			System.out.println("Couldn't parse: " + filepath + ".");
+			System.out.println("Couldn't parse: " + filepath + "!!!");
 			
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException e2) {
+
+				e2.printStackTrace();
+
+			}
 		}
 		
-		int i = 0;
-		
-		for (ArrayList<String> s : documentList){
-			
-			i += 1;
-			String num = Integer.toString(i);
-			System.out.println("Sentence"+ num + ":");
-			
-			for (String t : s){
-				System.out.println(t);
-				
+	for (ArrayList<String> s : documentList){
+		System.out.println("Sentence:");
+		for (String t : s){
+			System.out.println(t);
 			}
 		}
 	}
