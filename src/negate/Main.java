707e7940e67c6ef351c79be5046e5cb654c58cf9
kloehnen <kloehn@email.arizona.edu>
2017-03-07 15:53:56 -0700

    PR excel

--- src/negate/Main.java
+++ src/negate/Main.java
@@ -18,10 +18,10 @@ public class Main {
 		String dir = System.getProperty("user.dir");
 		
 		// declare paths
-		articlepath = dir + "/resources/documents/DoubleNegatives.txt";
+		articlepath = dir + "/resources/documents/goldstandard2.txt";
 		acceptpath = dir + "/resources/documents/Morphological_Negation/PorterMorphologicalNegationList.txt";
 		discardpath = dir + "/resources/documents/Morphological_Negation/ExemptionsToMorphologicalNegationList.txt";
-		outpath = dir + "/resources/documents/DNOutput.xml";
+		outpath = dir + "/resources/documents/TestOutput.xml";
 		
 		// initialize Text Read Object
 		TextRead text = new TextRead();
